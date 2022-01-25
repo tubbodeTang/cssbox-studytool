40945916f5ee29014155a64c080ab3f6776bc7ac
tubbodeTang <txb_dev@163.com>
2022-01-25 16:29:29 +0800

    chore: add unplugin components for global components import

--- src/main.js
+++ src/main.js
@@ -5,32 +5,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import {
-    Button,
-    Tabbar,
-    TabbarItem,
-    NavBar,
-    Progress,
-    Icon,
-    Col,
-    Row,
-    Popup
-    // Swipe,
-    // SwipeItem
-} from 'vant';
-
 const app = createApp(App);
-app.use(Button);
-app.use(Tabbar);
-app.use(TabbarItem);
-app.use(NavBar);
-app.use(Progress);
-app.use(Icon);
-app.use(Col);
-app.use(Row);
-app.use(Popup);
-// app.use(Swipe);
-// app.use(SwipeItem);
+
 app.use(router);
 app.use(store);
 app.mount('#app');
--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,21 @@ import path from 'path'
 import styleImport, {
   VantResolve
 } from 'vite-plugin-style-import';
-
+import {
+  VantResolver,
+} from 'unplugin-vue-components/resolvers'
+import Components from 'unplugin-vue-components/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     styleImport({
       resolves: [VantResolve()],
+    }),
+    Components({
+      resolvers: [
+        VantResolver(),
+      ]
     })
   ],
   resolve: {
