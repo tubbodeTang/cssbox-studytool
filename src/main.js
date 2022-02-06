c838fc37bd7e777a01d6fc3f86dfd94a52031bda
tubbodeTang <txb_dev@163.com>
2022-02-06 16:25:07 +0800

    feat: add hammerjs to project and test it

--- src/main.js
+++ src/main.js
@@ -4,9 +4,13 @@ import {
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueHammer from './utils/testHammer.ts'
 
 const app = createApp(App);
 
 app.use(router);
 app.use(store);
+app.use(VueHammer, {
+	threshold: 0,
+});
 app.mount('#app');
---
+++ src/views/creationCenter/addCreation.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <div id="app-content" v-hammer:swipe.horizontal="test"></div>
+        {{ msg }}
+        <div
+            class="content"
+            v-hammer:press="test2"
+            v-hammer:tap="test"
+            v-hammer:pan.left="test"
+            v-hammer:panstart="test2"
+            v-hammer:panend="test2"
+        ></div>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+let msg = ref('Welcome to Your Vue.js App')
+function test(e) {
+    console.log(e);
+}
+function test2(e) {
+    console.log('aaaa');
+}
+</script>
+
+<style scoped>
+.content {
+    width: 100px;
+    height: 200px;
+    background-color: red;
+}
+</style>
