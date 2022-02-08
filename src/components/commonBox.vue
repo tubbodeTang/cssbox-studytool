9b8d35ecd0a9b8e4f13957d8fb08af2a9f674c87
tubbodeTang <txb_dev@163.com>
2022-02-09 00:52:27 +0800

    feat: add common box pieces for creation module

---
+++ src/components/commonBox.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="boxD" :style="styleObject">This is a commonBox:{{ index }}</div>
+</template>
+
+<script setup>
+import { ref } from '@vue/reactivity';
+
+const props = defineProps({
+    styleObj: {
+        type: Object,
+        default: () => {
+            return {
+                width: '100px',
+                height: '100px',
+                backgroundColor: '#ccc'
+            }
+        }
+    },
+    index: {
+        type: Number,
+        default: () => {
+            return null
+        }
+    }
+})
+const styleObject = ref(props.styleObj)
+console.log(props.styleObj)
+</script>
+
+<style scoped>
+.boxD {
+}
+</style>
