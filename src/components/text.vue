b1c0b7c0ed6536aad5722c224a4b57cd2906362d
tubbodeTang <txb_dev@163.com>
2022-02-26 18:42:20 +0800

    feat: add text component's text insert

--- src/components/text.vue
+++ src/components/text.vue
@@ -1,9 +1,20 @@
-<template>
-   This is a inline content
-</template>
+<template>{{text}}</template>
 
 <script setup>
-
+const props = defineProps({
+   styleObj: {
+      type: Object,
+      default: () => {
+         return {}
+      }
+   },
+   text: {
+      type: String,
+      default: () => {
+         return 'This is a inline content'
+      }
+   }
+})
 </script>
 
 <style scoped>
